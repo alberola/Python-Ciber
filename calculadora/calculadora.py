4eda8351625be0c629711d7c65f75648adf53fe2
Alejandro Alberola Gopar <alejandroalberola140495@gmail.com>
2023-10-16 19:23:53 +0100

    Calculadora_1.0
    Calculadora finalizada

---
+++ calculadora/calculadora.py
@@ -0,0 +1,39 @@
+
+#Metodo para imprimir el menu
+def printMenu():
+    print("BIENVENIDO A LA CALCULADORA:")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
+
+#Definicion de variables
+number1 = int
+number2 = int
+
+#Metodo para preguntar los números a operar
+def askNumbers():
+    global number1
+    number1 = int(input("Introduce el primer número: "))
+    global number2
+    number2 = int(input("Introduce el segundo número: "))
+
+
+while True:
+    printMenu()
+    opt = int(input(""))
+    if opt == 1:
+        askNumbers()
+        print("La suma de " + str(number1) + " + " + str(number2) + " = " + str(number1+number2))
+    elif opt == 2:
+        askNumbers(number1, number2)
+        print("La resta de " +  str(number1) + " - " + str(number2) + " = " + str(number1 - number2))
+    elif opt == 3:
+        askNumbers(number1, number2)
+        print("La multiplicacion de " +  str(number1) + " + " + str(number2) + " = " + str(number1 + number2))
+    elif opt == 4:
+        askNumbers(number1, number2)
+        print("La division de " +  str(number1) + " / " + str(number2) + " = " + str(number1 / number2))
+    elif opt == 5 or opt > 5 or opt < 1:
+        break

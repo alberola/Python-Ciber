44c2ccbb5620367c55df6b53a60c2233c4b109fe
Alejandro Alberola Gopar <alejandroalberola140495@gmail.com>
2023-10-27 18:03:03 +0100

    SonarLint
    Práctica relacionada con el uso de la extensión SonalLint

---
+++ sonarLint/sonar.py
@@ -0,0 +1,13 @@
+def bad_idea ():
+    a = 10
+    b = 10
+    print(a is b)
+
+def bad_idea_2():
+    c = 10
+    d = 10
+    print(c is d)
+
+if __name__ == '__main__':
+    bad_idea()
+    bad_idea_2()
